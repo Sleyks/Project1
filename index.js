a8d21aa8c4b4a9f53b4575413d01f92dee88d0f3
unknown <illichov.dm@gmail.com>
2022-11-03 07:53:49 +0200

    project1

---
+++ index.js
@@ -0,0 +1,8 @@
+let star='*';
+      console.log(star);
+      console.log(star,star);
+      console.log(star,star,star);
+      console.log(star,star,star,star);
+      console.log(star,star,star,star,star);
+      console.log(star,star,star,star,star,star);
+      
